caad7239b44a4e054c74adcba9c597eca26e7d44
Javad Karabi <karabijavad@gmail.com>
2017-07-11 16:22:46 -0500

    wip

--- selinux_hll_yaml/__init__.py
+++ selinux_hll_yaml/__init__.py
@@ -7,4 +7,11 @@ import yaml
 if __name__ == "__main__":
     data = yaml.load(sys.stdin)
 
-    print(data)
+    for selinux_user in data.get('users', []):
+        print('(user {})'.format(selinux_user))
+
+    for selinux_role in data.get('roles', []):
+        print('(role {})'.format(selinux_role))
+
+    for selinux_type in data.get('types', []):
+        print('(type {})'.format(selinux_type))
